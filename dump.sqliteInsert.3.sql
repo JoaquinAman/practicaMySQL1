797d688998ab47acb3dd23bcc991284984e0e22c
Joaquin Aman <Joaquin Aman>
2021-11-30 23:31:12 -0300

    adding tema 1

---
+++ dump.sqliteInsert.3.sql
@@ -0,0 +1,16 @@
+-- TABLE
+CREATE TABLE curso(
+  codigo_de_curso int primary key not null,
+ 
+nombre Varchar(50) NOT NULL,
+descripcion varchar(50), 
+turno Varchar(50) NOT NULL
+, cupo int);
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
